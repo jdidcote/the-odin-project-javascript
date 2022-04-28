d796b33ec596e814c813b692654ead10b71d868c
jdidcote <joshua.didcote@outlook.com>
2022-04-28 22:18:57 +0100

    Added pop up form

--- library/app.js
+++ library/app.js
@@ -1,3 +1,11 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
 class Library {
   constructor() {
     this.books = [];
