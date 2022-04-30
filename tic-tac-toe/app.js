39b359d824b849a0027879acb37a50848f616e78
jdidcote <joshua.didcote@outlook.com>
2022-04-30 21:00:34 +0100

    added Gameboard class

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,1 +1,23 @@
-console.log("test");
+class Gameboard {
+  drawFreshBoard() {
+    const boardContainer = document.querySelector(".board-container");
+
+    for (let i = 0; i < 9; i++) {
+      const boardSquare = document.createElement("div");
+      boardSquare.setAttribute("data-gridNum", i);
+      boardSquare.classList.add("board-square");
+      boardContainer.appendChild(boardSquare);
+    }
+  }
+}
+
+class Player {}
+
+class Game {}
+
+function game() {
+  gameboard = new Gameboard();
+  gameboard.drawFreshBoard();
+}
+
+game();
